3217ea3a2a87feea55c89fed38ca43c002e3407b
Ned-magician <nedjat2021@gmail.com>
2025-12-11 13:32:51 +0200

    Update gulpFile.js

--- gulpFile.js
+++ gulpFile.js
@@ -1,43 +1,53 @@
 const { src, dest, watch, series } = require("gulp");
-const gulp = require("gulp");
-const useref = require("gulp-useref");
 const sass = require("gulp-sass")(require("sass"));
-// const dartSass = gulpSass(dartSass);
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const terser = require("gulp-terser");
-const changed = require("gulp-changed");
 const imagemin = require("gulp-imagemin");
 const rename = require("gulp-rename");
+const useref = require("gulp-useref");
+const changed = require("gulp-changed");
 
-//------SASS Task--------
+// Compile SCSS -> CSS
 function scssTask() {
-  return src("./src/assets/sass/**/*.scss", { sourcemaps: true })
-    .pipe(useref())
-    .pipe(changed("dist/scss"))
+  return src("src/assets/sass/**/*.scss", { sourcemaps: true })
     .pipe(sass().on("error", sass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(dest("./dist/css", { sourcemaps: "." }));
+    .pipe(dest("dist/css", { sourcemaps: "." }));
 }
 
-function htmlTask() {
-  return src("src/fitness.html").pipe(rename("index.html")).pipe(dest("dist"));
-}
-
-function imgTask() {
-  return src("src/assets/images/**/*").pipe(dest("dist/images"));
-}
-
+// Minify JS
 function jsTask() {
-  return src("src/assets/js/**/*.js").pipe(terser()).pipe(dest("dist/js"));
+  return src("src/assets/js/**/*.js")
+    .pipe(terser())
+    .pipe(dest("dist/js"));
 }
 
-//------Watch Task--------
+// Copy images
+function imgTask() {
+  return src("src/assets/images/**/*")
+    .pipe(imagemin())
+    .pipe(dest("dist/images"));
+}
+
+// Copy HTML and fix paths
+function htmlTask() {
+  return src("src/fitness.html")
+    .pipe(rename("index.html"))
+    // Replace ../dist/ paths with direct paths inside dist/
+    .pipe(useref())
+    .pipe(dest("dist"));
+}
+
+// Watch files for development
 function watchTask() {
-  watch(["src/assets/sass/**/*.scss"], series(scssTask));
+  watch("src/assets/sass/**/*.scss", scssTask);
+  watch("src/assets/js/**/*.js", jsTask);
+  watch("src/assets/images/**/*", imgTask);
+  watch("src/fitness.html", htmlTask);
 }
 
-//------ Default Gulp Task--------
+// Build task
 exports.build = series(htmlTask, scssTask, jsTask, imgTask);
 exports.default = series(exports.build, watchTask);
